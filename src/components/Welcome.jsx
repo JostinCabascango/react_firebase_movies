a78b88e4710678c41a4898c2afd735d0f6a55dcd
Jostin Fabian Cabascango Chavez <jostinfabian.cabascango@servosa.com>
2024-05-24 16:16:31 +0200

    feat: Add Welcome component for personalized greetings

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Welcome = ({ username }) => (
+  <div>
+    <h1>Hola, {username}!</h1>
+    <p>Películas para el verano</p>
+  </div>
+);
+
+export default Welcome;
