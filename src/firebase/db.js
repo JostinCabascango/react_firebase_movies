4dd0f344eced6d8cd2a9390d0bef2f93c78854d5
Jostin Fabian Cabascango Chavez <jostinfabian.cabascango@servosa.com>
2024-05-24 17:06:06 +0200

    feat: Add Firebase Firestore integration for movie data

---
+++ src/firebase/db.js
@@ -0,0 +1,25 @@
+import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
+import { app } from './firebase';
+
+const db = getFirestore(app);
+const moviesCollection = collection(db, 'movies');
+
+export const addMovie = async (movie) => {
+  try {
+    const docRef = await addDoc(moviesCollection, movie);
+    return docRef.id;
+  } catch (error) {
+    console.error("Error adding document: ", error);
+    throw error;
+  }
+};
+
+export const getMovies = async () => {
+  try {
+    const querySnapshot = await getDocs(moviesCollection);
+    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  } catch (error) {
+    console.error("Error getting documents: ", error);
+    throw error;
+  }
+};
