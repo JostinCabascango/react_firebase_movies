b34a979123316391332f67fccb144799df93cd2d
Jostin Fabian Cabascango Chavez <jostinfabian.cabascango@servosa.com>
2024-05-24 17:14:09 +0200

    refactor: Refactor addMovie and getMovies functions in firebase/db.js

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -5,21 +5,21 @@ const db = getFirestore(app);
 const moviesCollection = collection(db, 'movies');
 
 export const addMovie = async (movie) => {
-  try {
-    const docRef = await addDoc(moviesCollection, movie);
-    return docRef.id;
-  } catch (error) {
-    console.error("Error adding document: ", error);
-    throw error;
-  }
+    try {
+        const docRef = await addDoc(moviesCollection, movie);
+        return docRef.id;
+    } catch (error) {
+        console.error("Error adding document: ", error);
+        throw error;
+    }
 };
 
 export const getMovies = async () => {
-  try {
-    const querySnapshot = await getDocs(moviesCollection);
-    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-  } catch (error) {
-    console.error("Error getting documents: ", error);
-    throw error;
-  }
+    try {
+        const querySnapshot = await getDocs(moviesCollection);
+        return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    } catch (error) {
+        console.error("Error getting documents: ", error);
+        throw error;
+    }
 };
