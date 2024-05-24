28fa354c17b7ac5cf6ea4a512d90061bf60a8c18
Jostin Fabian Cabascango Chavez <jostinfabian.cabascango@servosa.com>
2024-05-24 17:05:51 +0200

    refactor: Update Welcome component to use Tailwind CSS classes for styling

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Welcome = ({ username }) => (
-  <div>
-    <h1>Hola, {username}!</h1>
-    <p>Películas para el verano</p>
-  </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-500 mb-4">Hola, {username}!</h1>
+        <p className="text-xl text-gray-700">Películas para el verano</p>
+    </div>
 );
 
 export default Welcome;
