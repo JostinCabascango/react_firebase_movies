24baa4ed0c59b38b457d093967908ed74794761e
Jostin Fabian Cabascango Chavez <jostinfabian.cabascango@servosa.com>
2024-05-24 16:41:39 +0200

    feat: Update import paths in App.jsx for better organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Welcome from '../Welcome';
-import MoviesList from '../../pages/MoviesList';
-import MoviesAdd from '../../pages/MoviesAdd';
+
+import Welcome from './components/Welcome';
+import MoviesList from './pages/MoviesList';
+import MoviesAdd from './pages/MoviesAdd';
 
 const App = () => (
   <Router>
