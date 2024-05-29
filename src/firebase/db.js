584c0d828278ced1601166b3c2d0870b47330625
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2024-05-29 17:22:45 +0200

    AÑADIR FUNCIONALIDADES EXTRA

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,25 +1,60 @@
-import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
-import { app } from './firebase';
+import {getFirestore, collection, addDoc, getDocs, doc, deleteDoc, setDoc, getDoc} from 'firebase/firestore';
+import {app} from './firebase';
 
 const db = getFirestore(app);
 const moviesCollection = collection(db, 'movies');
+
+const getMovieDoc = (id) => doc(db, 'movies', id);
+
+const handleFirestoreError = (error, operation) => {
+    console.error(`Error ${operation}: `, error);
+    throw error;
+};
 
 export const addMovie = async (movie) => {
     try {
         const docRef = await addDoc(moviesCollection, movie);
         return docRef.id;
     } catch (error) {
-        console.error("Error adding document: ", error);
-        throw error;
+        handleFirestoreError(error, 'adding document');
     }
 };
 
 export const getMovies = async () => {
     try {
         const querySnapshot = await getDocs(moviesCollection);
-        return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        return querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
     } catch (error) {
-        console.error("Error getting documents: ", error);
-        throw error;
+        handleFirestoreError(error, 'getting documents');
     }
 };
+
+export const updateMovie = async (id, updatedMovie) => {
+    try {
+        await setDoc(getMovieDoc(id), updatedMovie);
+    } catch (error) {
+        handleFirestoreError(error, 'updating document');
+    }
+};
+
+export const deleteMovie = async (id) => {
+    try {
+        await deleteDoc(getMovieDoc(id));
+    } catch (error) {
+        handleFirestoreError(error, 'deleting document')
+    }
+};
+
+export const getMovie = async (id) => {
+    try {
+        const docSnapshot = await getDoc(getMovieDoc(id));
+        if (docSnapshot.exists()) {
+            return {id: docSnapshot.id, ...docSnapshot.data()};
+        } else {
+            throw new Error('Movie not found');
+        }
+
+    } catch (error) {
+        handleFirestoreError(error, 'getting document');
+    }
+}
